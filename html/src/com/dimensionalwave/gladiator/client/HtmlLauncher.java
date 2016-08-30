7fc13b4c14434f33f6458d1eb13b3b548aa1bc47
Daniel McAssey <daniel.mcassey@gmail.com>
2016-08-30 02:00:26 +0100

    Fix compilation errors

--- html/src/com/dimensionalwave/gladiator/client/HtmlLauncher.java
+++ html/src/com/dimensionalwave/gladiator/client/HtmlLauncher.java
@@ -3,13 +3,14 @@ package com.dimensionalwave.gladiator.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.dimensionalwave.gladiator.Constants;
 import com.dimensionalwave.gladiator.GladiatorGame;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(Constants.V_WIDTH * Constants.SCALE, Constants.V_HEIGHT * Constants.SCALE);
         }
 
         @Override
